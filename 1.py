4af0e135c6d94f5deaffafb54f792f6c686d982e
ORION9k <82353870+ORION9k@users.noreply.github.com>
2023-07-27 08:52:25 -0300

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+print('Olá, seja nem vindo á Woodsvile.')
+print('A história interativa criada por ORION FREITAS em phyton.')
+print('Na realidade, este projeto foi criado para iniciar os conhecimentos de seu autor na linguagem')
+print('Versão 0.0.1')
+
+nome = input('Escolha o nome de seu personagem ')
+idade = input('Escolha a idade de seu personagem ')
+print('Ok, nessa história você será,', nome, ' e terá', idade, 'anos.')
