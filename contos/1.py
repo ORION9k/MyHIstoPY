0f74845b224a0ed53a071177f04607d270dfbf5d
ORION9k <82353870+ORION9k@users.noreply.github.com>
2023-08-07 11:47:15 -0300

    Added emojis

--- contos/1.py
+++ contos/1.py
@@ -1,12 +1,15 @@
+import emoji
+
 # Informações & Versões
-print('Olá, seja nem vindo á Woodsvile.')
-print('A história interativa criada por ORION FREITAS em phyton.')
-print('Este projeto foi criado para iniciar os conhecimentos de seu autor na linguagem')
-print('Versão 0.0.1')
-print('Algumas das escolhas modificam o andamento da história')
+print(emoji.emojize('Olá, seja nem vindo á Woodsvile. :church:'))
+print(emoji.emojize('\n:writing_hand:   A história interativa criada por ORION FREITAS em phyton.'))
+print('    Este projeto foi criado para iniciar os conhecimentos de seu autor na linguagem')
+print(emoji.emojize('\n:construction:  Versão 0.0.1'))
+print(emoji.emojize('\n:shuffle_tracks_button:  Algumas das escolhas modificam o andamento da história! \n'))
 
 # Personagem
-nome = input('Escolha o nome de seu personagem ')
-idade = input('Escolha a idade de seu personagem ')
+nome = input('Escolha o nome de seu personagem: ')
+idade = input('Escolha a idade de seu personagem: ')
 print(f'Ok, nessa história você será {idade} e terá {nome} anos.') 
+
 
